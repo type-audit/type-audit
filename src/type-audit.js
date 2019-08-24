93125e2a97fe81313e37c880b63d14ae54ceb1d0
victor <victor@ma.il>
2019-08-24 16:36:05 +0700

    Used "err.js" module for errors functions

--- src/type-audit.js
+++ src/type-audit.js
@@ -1,4 +1,5 @@
 import Is from './is';
+import * as Err from './err';
 
 
 
@@ -45,65 +46,13 @@ const argLevels = checkers.reduce((result, method) => {
 
 
 
-/**
- * @protected
- * @param {function|string} naming Наименование при ошибке (строка или функция, возвращающая строку)
- * @param {function|string} type Тип проверяемого значения
- * @param {*} value Проверяемое значение
- * @param {boolean} [isRequired] Значение null или undefined не допускается (опционально)
- * @return {string}
- */
-const _errMessage = (naming, type, value, isRequired) => {
-    let name;
-    if (Is.function(naming, true)) {
-        name = naming();
-    }
-    else
-    if (Is.notEmptyString(naming, true)) {
-        const i = naming.indexOf(':');
-        if (0 < i && i < naming.length - 1) {
-            // Если двоеточие есть, и это не первый и не последний символ, то:
-            const key = naming.substring(0, i);
-            name = key === 'arg'
-                ? `Argument "${naming.substring(i + 1)}"`
-                : `${key.charAt(0).toUpperCase()}${key.substring(1)} "${naming.substring(i + 1)}"`;
-        }
-        else {
-            name = naming;
-        }
-    }
-    else {
-        throw new TypeError(`Wrong argument "naming": ${naming}`);
-    }
-    return `${name} ${isRequired ? 'must be' : 'can be only'} ${typeof type === 'function' ? type() : type}: ${value}`;
-};
-
-/**
- * @protected
- * @param {string} message Текст сообщения об ошибке
- * @return {Error}
- */
-const _error = (message) => {
-    let err = new TypeError(message);
-    const stack = err.stack.split(/[\r\n]+/);
-    stack.splice(stack[0].indexOf(message) !== -1 ? 1 : 0, 2);
-    if (err.fileName !== undefined && err.lineNumber !== undefined) {
-        const where = stack[0].match(/@([^\s]+):([0-9]+):([0-9]+)$/i);
-        err = new TypeError(message, where[1], +where[2], +where[3]);
-    }
-    err.stack = stack.join('\n');
-    return err;
-};
-
-
-
 const _propGetChecker = (method, typeInfo, isRequired) => (props, propName, componentName) => {
     const needTypeInfo = 0 < argLevels[method];
     const checker = Is[method];
     const value = props[propName];
     if (needTypeInfo ? !checker(value, typeInfo, isRequired) : !checker(value, isRequired)) {
         const type = TYPE_NAMES[method];
-        return _error(_errMessage(
+        return Err.create(Err.makeMessage(
             `Prop "${propName}" in component "${componentName}"`,
             needTypeInfo ? type(typeInfo) : type, value, isRequired
         ));
@@ -156,7 +105,7 @@ checkers.forEach((method) => {
          */
         checker = (value, typeInfo, naming, isRequired) => {
             if (!Is[method](value, typeInfo, isRequired)) {
-                throw _error(_errMessage(naming, TYPE_NAMES[method](typeInfo), value, isRequired));
+                throw Err.create(Err.makeMessage(naming, TYPE_NAMES[method](typeInfo), value, isRequired));
             }
         };
     }
@@ -169,7 +118,7 @@ checkers.forEach((method) => {
          */
         checker = (value, naming, isRequired) => {
             if (!Is[method](value, isRequired)) {
-                throw _error(_errMessage(naming, TYPE_NAMES[method], value, isRequired));
+                throw Err.create(Err.makeMessage(naming, TYPE_NAMES[method], value, isRequired));
             }
         };
     }
